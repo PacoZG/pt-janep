408f9364bdee289c2b6e4f96785ba2f722069f00
pacozavala <paco.zavala@houston-inc.com>
2025-09-06 13:47:24 +0300

    Refactor AboutSection for improved layout, styling, and background image

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -11,17 +11,17 @@ const AboutSection: FC = () => {
   return (
     <section
       id="about"
-      className="flex flex-row items-center relative overflow-hidden bg-gray-600 bg-opacity-40 h-[500px] md:h-[1024px]"
+      className="flex flex-row items-center relative overflow-hidden bg-gray-600 bg-opacity-40 h-[640px] md:h-[1024px]"
     >
-      <Container>
+      <Container className="px-1">
         <div
           className="absolute inset-0 bg-cover bg-center"
           style={{
-            backgroundImage: `url(https://res.cloudinary.com/dbn5gpgi5/image/upload/v1755421573/vkci2d0mcqxa6o6cbt1r.jpg)`,
+            backgroundImage: `url(https://res.cloudinary.com/dbn5gpgi5/image/upload/v1755427123/kxcvzzebkqers0gx3zh3.jpg)`,
           }}
         />
 
-        <div className="relative z-10 w-full p-8">
+        <div className="relative z-10 w-full p-8 text-gray-300">
           <SectionTitle
             title={t('about.title')}
             subtitle={t('about.subtitle')}
@@ -30,8 +30,8 @@ const AboutSection: FC = () => {
           <div className="md:px-24 mx-auto mt-8 grid max-w-4xl gap-2 bg-transparent">
             {aboutBullets.map(i => (
               <div key={i} className="flex items-center gap-2 text-base">
-                <Check className="mt-1 h-4 w-4 flex-none text-blue-950" />
-                <span className="text-gray-900">{t(`about.bullets.${i}`)}</span>
+                <Check className="mt-1 h-4 w-4 flex-none" />
+                <span className="">{t(`about.bullets.${i}`)}</span>
               </div>
             ))}
           </div>
